b0f78ffeee856db02dc608aad1a93caec4157131
DiomarBR <diomarbr4@gmail.com>
2022-11-19 21:01:07 -0300

    commit inicial

---
+++ aula 1/teste.py
@@ -0,0 +1,84 @@
+import pyautogui
+import pyperclip
+import time
+
+pyautogui.PAUSE = 1
+
+# pyautogui.click -> clicar
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.hotkey -> conjunto de teclas
+# pyautogui.write -> escreve um texto
+
+# Passo 1: Entrar no sistema da empresa (no nosso caso é o link do drive)
+pyautogui.hotkey("ctrl", "t")
+pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+
+time.sleep(5)
+
+# Passo 2: Navegar no sistema e encontrar a base de vendas (entrar na pasta exportar)
+pyautogui.click(x=330, y=287, clicks=2)
+time.sleep(6)
+
+# Passo 3: Fazer o download da base de vendas
+pyautogui.click(x=390, y=279) # clicar no arquivo
+pyautogui.click(x=1154, y=185) # clicar nos 3 pontinhos
+pyautogui.click(x=1003, y=625) # clicar no fazer download
+time.sleep(5) # esperar o download acabar
+
+# Passo 4: Importar a base de vendas pro Python
+import pandas as pd
+
+tabela = pd.read_excel(r"C:\Users\Diomar Gonçalves\Downloads\Vendas - Dez.xlsx")
+
+
+# Passo 5: Calcular os indicadores da empresa
+faturamento = tabela["Valor Final"].sum()
+print(faturamento)
+quantidade = tabela["Quantidade"].sum()
+print(quantidade)
+
+# Passo 6: Enviar um e-mail para a diretoria com os indicadores de venda
+
+# abrir aba
+pyautogui.hotkey("ctrl", "t")
+
+# entrar no link do email - https://mail.google.com/mail/u/0/#inbox
+pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+time.sleep(5)
+
+# clicar no botão escrever
+pyautogui.click(x=78, y=192)
+time.sleep(5)
+# preencher as informações do e-mail
+pyautogui.write("biangulowalysson@gmail.com")
+pyautogui.press("tab") # selecionar o email
+
+pyautogui.press("tab") # pular para o campo de assunto
+pyperclip.copy("Relatório de Vendas")
+pyautogui.hotkey("ctrl", "v")
+
+pyautogui.press("tab") # pular para o campo de corpo do email
+
+texto = f"""
+Prezados,
+
+Segue relatório de vendas.
+Faturamento: R${faturamento:,.2f}
+Quantidade de produtos vendidos: {quantidade:,}
+
+Qualquer dúvida estou à disposição.
+Att.,
+Lira do Python
+"""
+
+# formatação dos números (moeda, dinheiro)
+
+pyperclip.copy(texto)
+pyautogui.hotkey("ctrl", "v")
+
+# enviar o e-mail
+pyautogui.hotkey("ctrl", "enter")
--- teste.py
+++ teste.py
@@ -1,91 +1,51 @@
-import pyautogui
-import pyperclip
-import time
+from selenium import webdriver #criar o navegador
+from selenium.webdriver.common.keys import Keys#permite escrever no navegador
+from selenium.webdriver.common.by import By#permite você selecionar itens no navegador
+#abrir o navegador
+navegador = webdriver.Chrome()
+
+#passo 1: Pegar a acotação do dólar
+
+#entrar no google
+navegador.get("https://google.com.br")
+
+#pesquisar no google a "cotação do dolar"
+navegador.find_element("xpath", '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("Cotação dolar")
+navegador.find_element("xpath", '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+#pegar a cotação que tá no google
+cotacao_dolar = navegador.find_element("xpath", '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+
+#passo 2: Pegar a cotação do euro
+navegador.get("https://google.com.br")
+navegador.find_element("xpath", '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("Cotação euro")
+navegador.find_element("xpath", '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+cotacao_euro = navegador.find_element("xpath", '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+
+#passo 3: Pegar a cotação do ouro
+navegador.get("https://www.melhorcambio.com/ouro-hoje")
+cotacao_ouro = navegador.find_element("xpath", '//*[@id="comercial"]').get_attribute('value')
+cotacao_ouro = cotacao_ouro.replace(",",".")
+
+navegador.quit()
+
+#passo 4: Atualizar a base de preços (atualizando o preço de compra e o de venda)
 import pandas as pd
-
-pyautogui.PAUSE = 1
-
-# pyautogui.click -> clicar
-# pyautogui.press -> apertar 1 tecla
-# pyautogui.hotkey -> conjunto de teclas
-# pyautogui.write -> escreve um texto
-
-# Passo 1: Entrar no sistema da empresa (no nosso caso é o link do drive)
-pyautogui.click(x=496, y=747)
-time.sleep(5)
-pyperclip.copy("Chrome")
-pyautogui.hotkey("ctrl","V")
-pyautogui.press("enter")
-
-pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.press("enter")
-
-time.sleep(5)
-
-# Passo 2: Navegar no sistema e encontrar a base de vendas (entrar na pasta exportar)
-pyautogui.click(x=330, y=287, clicks=2)
-time.sleep(6)
-
-# Passo 3: Fazer o download da base de vendas
-pyautogui.click(x=390, y=279) # clicar no arquivo
-pyautogui.click(x=1154, y=185) # clicar nos 3 pontinhos
-pyautogui.click(x=934, y=626) # clicar no fazer download
-time.sleep(5) # esperar o download acabar
-
-
-
-# Passo 4: Importar a base de vendas pro Python
-
-tabela = pd.read_excel(r"C:\Users\Diomar Gonçalves\Downloads\Vendas - Dez.xlsx")
+tabela=pd.read_excel("Produtos.xlsx")
 print(tabela)
 
-# Passo 5: Calcular os indicadores da empresa
-faturamento = tabela["Valor Final"].sum()
-print(faturamento)
-quantidade = tabela["Quantidade"].sum()
-print(quantidade)
+#atualizar a coluna de cotação
+#eu quero editar a coluan cotaçao onde a moeda e = dolar
+tabela.loc[tabela["Moeda"] == "Dólar", "Cotação"] = float(cotacao_dolar)
+tabela.loc[tabela["Moeda"] == "Euro", "Cotação"] = float(cotacao_euro)
+tabela.loc[tabela["Moeda"] == "Ouro", "Cotação"] = float(cotacao_ouro)
 
-# Passo 6: Enviar um e-mail para a diretoria com os indicadores de venda
+#atualizar a coluna de preço de compra = preço original * cotação
+tabela["Preço de Compra"] = tabela["Preço Original"] * tabela["Cotação"]
 
-# abrir aba
-pyautogui.hotkey("ctrl", "t")
+#atualizar a coluna de venda = preço de compra * margen
+tabela["Preço de Venda"] = tabela["Preço de Compra"] * tabela["Margem"]
 
-# entrar no link do email - https://mail.google.com/mail/u/0/#inbox
-pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.press("enter")
-time.sleep(5)
+print(tabela)
 
-# clicar no botão escrever
-pyautogui.click(x=78, y=192)
-time.sleep(5)
-# preencher as informações do e-mail
-pyautogui.write("lissamely03@gmail.com")
-pyautogui.press("tab") # selecionar o email
-
-pyautogui.press("tab") # pular para o campo de assunto
-pyperclip.copy("Relatório de Vendas")
-pyautogui.hotkey("ctrl", "v")
-
-pyautogui.press("tab") # pular para o campo de corpo do email
-
-texto = f"""
-Prezados,
-
-Segue relatório de vendas.
-Faturamento: R${faturamento:,.2f}
-Quantidade de produtos vendidos: {quantidade:,}
-
-Qualquer dúvida estou à disposição.
-Att.,
-Lira do Python
-"""
-
-# formatação dos números (moeda, dinheiro)
-
-pyperclip.copy(texto)
-pyautogui.hotkey("ctrl", "v")
-
-# enviar o e-mail
-pyautogui.hotkey("ctrl", "enter")
+#passo 5: Exportar a base de preços atualizados
+tabela.to_excel("Produtos Novo.xlsx", index=False)
